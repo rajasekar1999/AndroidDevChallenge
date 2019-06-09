8cdd7a1638d61027262dbd4f1ea94a04e1e20a7d
Rajasekar M <36572895+rajasekar1999@users.noreply.github.com>
2019-06-09 16:06:29 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/buttonapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.buttonapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Button b1;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Do it Dhoni!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
